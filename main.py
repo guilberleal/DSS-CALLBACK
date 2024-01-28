362e4dc780f78b78173824d36151409572d7f6d8
Guilber leal <guilber443@gmail.com>
2024-01-28 17:53:23 -0300

    AtualizacaoV5

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import pyperclip
 import pyautogui as pg
 import threading
 import time
-import banco
 
 
 
@@ -23,12 +22,11 @@ def comecar():
                                                                   Sugestões de Melhorias nas descrições de chamados
                         
      Informe o tipo de script que deseja gerar.
-    1 = CALLBACK
-    2 = Adicionar 0
-    3 = Tentativa de contato
-    4 = Encerramento após 3 tentativas
+    1 = Adicionar 0
+    2 = Tentativa de contato
+    3 = Encerramento após 3 tentativas
           
-    5 = Banco config""")
+    """)
 
     tiposcript = str(input('  '))
     os.system('cls') or None
@@ -37,9 +35,6 @@ def comecar():
 def analise_script(tiposc):
     
     if tiposc == '1':
-        return solucao()
-    
-    elif tiposc == '2':
         numero = str(input(' Número de contato:'))
         os.system('cls') or None
         texto = (f'0{numero}')
@@ -47,16 +42,11 @@ def analise_script(tiposc):
         colar(texto)
         return comecar()
 
-    elif tiposc == '3':
+    elif tiposc == '2':
         return tentativa()
        
-    elif tiposc == '4':
+    elif tiposc == '3':
         return encerramento3()
-
-
-    elif tiposc == '5':
-        banco.configb()
-        return comecar()
 
     elif tiposc == 'sair':
         return sys.exit("Encerrando ...........")
@@ -68,40 +58,22 @@ def analise_script(tiposc):
         os.system('cls') or None
         return comecar()
 
-def solucao():
-    print(' Digite "voltar" para retornar à primeira página.\n')
-    
-    pessoa = str(input(' Nome do cliente:'))
-    numero = str(input(' Número de contato:'))
-    genero = banco.iniciarbc(pessoa)
-    genp = gen(pessoa, genero)  # Passando o gênero para a função gen
-    os.system('cls') or None
-    texto = (f"""CALLBACK REALIZADO COM SUCESSO. 
-Em contato por telefone ({numero}) com {genp} validou a solução do chamado. Sendo assim, autorizou o encerramento do chamado.
-Prezado(a), Sua opinião nos ajudará a melhorar ainda mais nossos serviços e qualidade no atendimento, portanto será enviado um SMS/E-mail com nossa pesquisa de satisfação para que avalie, desde já agradecemos!""")
-    print(texto)
-    colar(texto)
-    return comecar()
-
-
 def tentativa():
-    numero = str(input(' Número de contato:'))
-    os.system('cls') or None
-    texto = (f"""Tentativa de contato com solicitante através do telefone: {numero} que foi informado no chamado. A ligação chamou até cair/caiu na caixa postal.""")
-    print(texto)
-    colar(texto)
-    input(' \nVoltar ao começo')
-    os.system('cls') or None
+    
+    num = pyperclip.paste()
+    
+    pyperclip.copy(f"""Tentativa de contato com solicitante através do telefone: {num} que foi informado no chamado. A ligação chamou até cair/caiu na caixa postal.""")
+    
     return comecar()
 
 def encerramento3():
     numero = str(input(' Número do chamado:'))
     email = str(input(' E-mail do cliente:'))
+    num = str(input(' numero do cliente:'))
     os.system('cls') or None
     texto = (f"""Prezado cliente,
 O chamado/ticket {numero} está sendo encerrado por falta de contato, após 3 
-tentativas por telefone e 3 notific02
-ações enviados ao e-mail {email} para atendermos 
+tentativas por telefone {num}, e 3 notificações enviados ao e-mail {email}, para atendermos 
 ao seu chamado/ticket.
 Sendo assim, havendo o interesse deverá abrir um novo chamado/ticket, renovando-se todos 
 os prazos de atendimento.
@@ -129,14 +101,6 @@ def colar(texto):
     pg.press('enter')
     
     return comecar()
-
-
-def gen(pessoa, genero):
-    pessoa = pessoa.capitalize()
-    if genero == 'f':
-        return f'a senhora {pessoa}, a mesma'
-    else:
-        return f'o senhor {pessoa}, o mesmo'
 
 def acao_eventoCallback():
     global num, nome
